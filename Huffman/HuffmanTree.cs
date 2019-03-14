8f7aa1fe7ec89f862720e5564af998f87fd9b168
Максим Найдович <max.naid@mail.ru>
2019-03-14 17:37:02 +0500

    Добавьте файлы проекта.

---
+++ Huffman/HuffmanTree.cs
@@ -0,0 +1,49 @@
+﻿using Huffman;
+using System.Collections.Generic;
+using System.IO;
+
+public class HuffmanTree
+//этот класс должен строить дерево на основании файла
+//для начала нужно определить частоту каждого символа
+{
+    List<char> _leaves;
+    public HuffmanTree()
+    {
+
+    }
+
+    public List<Node> makeTree(string fileName)
+    {
+        List<Node> list = new List<Node>();
+
+        using (StreamReader sr = File.OpenText(fileName))
+        {
+            string line;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                foreach (char item in line)
+                {
+                    int value = 1;
+                    Node node = new Node(item, value);
+
+                    if (list.Contains(node))
+                    {
+                        value = list.Find(x => x.Symbol == item).Value;
+                        list.Remove(node);
+                        value++;
+                        node = new Node(item, value);
+                    }
+
+
+                    list.Add(node);
+                }
+
+            }
+        }
+
+        return list;
+    }
+
+    public List<char> Leaves { get => _leaves; set => _leaves = value; }
+}
---
+++ Huffman/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Huffman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "Письмо.txt";
+
+            var charsInFile = new HuffmanTree().makeTree(fileName);
+
+
+
+            foreach (var node in charsInFile)
+            {
+                Console.WriteLine(node.Symbol + ":" + node.Value);
+
+            }
+
+            //Node node1 = new Node('d', 3);
+            //Node node2 = new Node('d', 4);
+
+            //List<Node> list = new List<Node>();
+            //list.Add(node1);
+
+            //Console.WriteLine(list.Contains(node2));
+
+        }
+    }
+}
